942b04e73e9a9be52b856eb474d8937b27840fa5
GaurangDhorda <dg.dhorda@gmail.com>
2020-01-30 16:04:59 +0530

    Updates

---
+++ precache-manifest.7f8a9c8785acdf432ce96a21f66cbcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdcb2ef9574854df83e4f74116547ec5",
+    "url": "/react-pwa/index.html"
+  },
+  {
+    "revision": "6bd15d1428407466af4e",
+    "url": "/react-pwa/static/css/main.44bae32d.chunk.css"
+  },
+  {
+    "revision": "04aa6545ab02b29b4efa",
+    "url": "/react-pwa/static/js/2.255a4916.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-pwa/static/js/2.255a4916.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bd15d1428407466af4e",
+    "url": "/react-pwa/static/js/main.746cb255.chunk.js"
+  },
+  {
+    "revision": "aba18d6b6aaeda3a55cf",
+    "url": "/react-pwa/static/js/runtime-main.351b5433.js"
+  }
+]);
